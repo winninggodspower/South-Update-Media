0a11a3e1ee5c6785edac9a424234b9a5f1f93069
winning godspower <winninggodspower@gmail.com>
2024-02-06 09:58:31 +0100

    modify: getting settings file from .env

--- SouthUpdateMedia/wsgi.py
+++ SouthUpdateMedia/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SouthUpdateMedia.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"SouthUpdateMedia.settings.{os.getenv('ENVIRONMENT')}")
 
 application = get_wsgi_application()
