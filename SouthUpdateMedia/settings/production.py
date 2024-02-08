2c84ecc59542457b0780dfa5235a2abfede581a2
winning godspower <winninggodspower@gmail.com>
2024-02-08 10:56:19 +0100

    fix: add secret key and allowed host to production setting

--- SouthUpdateMedia/settings/production.py
+++ SouthUpdateMedia/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["southupdatemedia.com", "www.southupdatemedia.com"]
+
 try:
     from .local import *
 except ImportError:
