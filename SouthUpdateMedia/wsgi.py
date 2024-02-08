07c974547ec38ae8f912c3adef451b99666b359a
winning godspower <winninggodspower@gmail.com>
2024-02-08 11:10:37 +0100

    feat: add dotenv to wsgi file

--- SouthUpdateMedia/wsgi.py
+++ SouthUpdateMedia/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 from django.core.wsgi import get_wsgi_application
 
